23f328a8bd3b457ab121177629c548f7374f795f
EliasYoc <eliyoc_97@hotmail.com>
2021-09-02 17:21:46 -0500

    upload de videos

--- main.js
+++ main.js
@@ -4,11 +4,8 @@ const $preview = d.getElementById("preview");
 
 d.addEventListener("change", (e) => {
   if (e.target === $inputFile) {
-    console.log(e);
     Array.from(e.target.files).forEach((file) => {
-      console.log("arriba d funct upload", file);
       uploadFile(file);
-      console.log("abajo d funct upload");
     });
   }
 });
@@ -21,15 +18,16 @@ const uploadFile = (file) => {
   const $video = d.createElement("video");
   $video.className = "card__video";
   $video.controls = true;
-  $video.src = URL.createObjectURL(file); //para previsualizar video mientras carga pero consume
+  $video.src = URL.createObjectURL(file); //para previsualizar video mientras carga(con videos de 1 hora), 🤷‍♂️
   $article.insertAdjacentElement("beforebegin", $video);
   const size = file.size;
 
   const name = file.name.substring(0, 20);
   const reader = new FileReader();
   reader.readAsDataURL(file);
-  console.log("dentro upload", reader);
+
   reader.addEventListener("progress", (e) => {
+    //cargando...
     const percentageNum = parseInt((e.loaded * 100) / e.total);
     const bytesUploading = (e.loaded * size) / e.total;
     const mbUploading = bytesUploading / MB;
@@ -40,11 +38,9 @@ const uploadFile = (file) => {
     <p class="card__title">${name}... </p>
     <progress value="${percentageNum}" max="100"></progress>
 `;
-
-    // console.log(reader.readyState);
-    // console.log("dentro reader progress", e); //se ejecuta al final
   });
   reader.addEventListener("loadend", (e) => {
     URL.revokeObjectURL(file);
+    //subirlo al server...
   });
 };
